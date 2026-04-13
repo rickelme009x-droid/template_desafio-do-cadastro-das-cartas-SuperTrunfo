7b7c0bf4de701a606ee84d585918280f77d490b2
rickelme009x-droid <rickelme009x@gmail.com>
2026-04-13 14:19:51 +0000

    Finalizei o Desafio Mestre: Cadastro de Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,12 +10,13 @@ int main() {
   char estado1;
   char codigo1[4];
   char cidade1[50]; 
-  int populacao1; 
+  unsigned int populacao1; 
   double area1; 
   double pib1; 
   int pontos1;
   double densidadepop1;
   double pib_per_capita1;
+  float superpoder1 = 
   
   // Cadastro da Carta 2
    char estado2;
@@ -27,6 +28,7 @@ int main() {
   int pontos2;
   double densidadepop2;
   double  pib_per_capita2;
+  float superpoder2;
 
   // Área para entrada de dados
   // CARTA 1
@@ -53,10 +55,14 @@ int main() {
   printf("Digite o total de pontos turísticos:\n ");
   scanf("%d", &pontos1);
   
+  //Cálculo da Densidade e do PIB Per Capita
   densidadepop1 = (double) populacao1 / area1;
 
   pib_per_capita1 = ( pib1 * 1000000000 ) / populacao1;
 
+  //Super Poder da Carta1
+    super1 = (float)populacao + area + pib + pontosturisticos + pib1_1 + (1 / densidade1);
+    
   //CARTA 2
   
   printf("Cadastro da Carta 2\n ");
@@ -81,10 +87,13 @@ int main() {
   printf("Digite o total de pontos turísticos:\n ");
   scanf("%d", &pontos2);
   
+  //Cálculo da Densidade e do PIB Per Capita
   densidadepop2 = (double) populacao2 / area2;
 
   pib_per_capita2 = ( pib2 * 1000000000 ) / populacao2;
 
+  //Super Poder da Carta2
+  super2 = (float)populacao2 + area2 + pib2 + pontosturisticos2 + pib2_2 + (1 / densidade2);
 
   // Área para exibição dos dados da cidade
   // Exibição da Carta 1
